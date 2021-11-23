165b554c549a2bdd2f5b7be2a8d1880aea4fab87
Oliver Wisler <oliver@oliverwisler.ch>
2021-11-23 23:11:51 +0100

    Fixing comments

--- okdmr/tests/kaitai/hytera/test_text_message_protocol.py
+++ okdmr/tests/kaitai/hytera/test_text_message_protocol.py
@@ -6,9 +6,9 @@ from okdmr.tests.kaitai.tests_utils import prettyprint
 
 def test_lp():
     hexmessages: List[str] = [
-        # text message?
+        # this is not a text message?
         "a0030034000000000a2338630000413131323534303237303932314e353030332e383734364530313432362e35313932302e32323533ff1cae03",
-        # Is this really a txt message?
+        # Is this really a txt message
         "0980a10022000000010a01b2070a03640e4f004c004900560045005200200054004500530054007a03"
     ]
     for hexmsg in hexmessages:
